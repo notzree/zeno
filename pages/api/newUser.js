9a0b2af0560f28c1739da1e49374098ac851bb63
notzree <r29zhang@uwaterloo.ca>
2023-01-11 10:27:15 -0500

    bug fixs

--- pages/api/newUser.js
+++ pages/api/newUser.js
@@ -1,6 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Prisma, PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const userData = JSON.parse(req.body);
